85993036794756c155692816870f99af240a7932
JamesWClark <jwclark@rockhursths.edu>
2016-03-27 16:38:14 -0500

    add file select

--- main.js
+++ main.js
@@ -1,22 +1,76 @@
 /* JUST WOW - https://www.tinymce.com/ */
 
+var editor;
+var numFiles = 0;
+
+var verifyFileAPISupport = function() {
+    // Check for the various File API support.
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+      // Great success! All the File APIs are supported.
+    } else {
+      $('#support-message').text('The File APIs are not fully supported in this browser.');
+    }    
+};
+
+var processFile = function(event, file) {
+    var code = event.target.result;
+    console.log(file.name);
+    setNewSketch(code);
+    setEditorCode(code);
+};
+
+var processFiles = function(files) {
+    numFiles = files.length;
+    for(var i = 0; i < files.length; i++) {
+        var f = files[i];
+        var reader = new FileReader();
+        reader.onloadend = (function(file) {
+            return function(event) {
+                processFile(event, file);
+            };
+        })(f);
+        reader.readAsText(f);
+    }
+};
+
+var onFilesSelected = function(event) {
+    var files = event.target.files; // FileList object
+    processFiles(files);
+};
+
+var registerHandlers = function() {
+    document.getElementById('files').addEventListener('change', onFilesSelected, false);
+};
+
+var setEditorCode = function(code) {
+    editor.getSession().setValue(code);
+};
+
+var setNewSketch = function(code) {
+    Processing.getInstanceById('sketch').exit();
+    var container = $('#canvas-container');
+    var canvas = document.createElement('canvas');
+    canvas.id = 'sketch';
+    container.html('');
+    container.append(canvas);
+    new Processing(canvas, code);
+};
+
 $(document).ready(function() {
+    verifyFileAPISupport();
+    registerHandlers();
+    
     var initcode = $('#processing-code').text();
-    var editor = ace.edit("editor");
+    editor = ace.edit("editor");
     editor.$blockScrolling = Infinity;
     editor.setTheme("ace/theme/monokai");
     editor.getSession().setMode("ace/mode/java");
-    editor.getSession().setValue(initcode);
+    
+    setEditorCode(initcode);
     
     $('#run').click(function() {
-        Processing.getInstanceById('sketch').exit();
         var code = editor.getSession().getValue();
-        var container = $('#canvas-container');
-        var canvas = document.createElement('canvas');
-        canvas.id = 'sketch';
-        container.html('');
-        container.append(canvas);
-        new Processing(canvas, code);
+        setNewSketch(code);
     });
 });
 
