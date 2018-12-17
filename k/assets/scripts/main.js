063e35a0e51509a616af6553360dcdd8996319f5
JW Clark <jwclark@rockhursths.edu>
2018-12-17 10:54:39 -0600

    Fix indentation

--- k/assets/scripts/main.js
+++ k/assets/scripts/main.js
@@ -13,10 +13,8 @@ grader.handleDrops('files');
 grader.writeTo('result');
 
 $('#result').on('click', '#scorecard-students-table tr th, #scorecard-students-table tr td', function () {
-
-        $('*').removeClass('selected');
-        $(this).addClass('selected');
-        var columnNo = $(this).index();
-        $(this).closest("table").find("tr td:nth-child(" + (columnNo + 1) + ")").addClass('selected');
-
+    $('*').removeClass('selected');
+    $(this).addClass('selected');
+    var columnNo = $(this).index();
+    $(this).closest("table").find("tr td:nth-child(" + (columnNo + 1) + ")").addClass('selected');
 });
